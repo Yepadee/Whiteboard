26f6aa4fc62b3766ead778cdd1b1890b59ff35eb
James <james@hawkinsplace.co.uk>
2018-12-06 12:56:30 +0000

    Shifted all files up a folder to see if that works.

--- WhiteboardSpring/src/main/java/org/microboard/whiteboard/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.microboard.whiteboard.repositories;
-
-import org.microboard.whiteboard.model.user.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-	
-}
---
+++ src/main/java/org/microboard/whiteboard/WhiteboardSpringApplication.java
@@ -0,0 +1,12 @@
+package org.microboard.whiteboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteboardSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteboardSpringApplication.class, args);
+	}
+}
