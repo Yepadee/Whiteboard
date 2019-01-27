36cdc75898d11704dd06212f4b5ff9070742ad36
James <james@hawkinsplace.co.uk>
2019-01-27 20:06:16 +0000

    Added remaining service tests (unimplemented)

---
+++ src/test/java/org/microboard/whiteboard/service_tests/AssessorServiceTest.java
@@ -0,0 +1,14 @@
+package org.microboard.whiteboard.service_tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AssessorServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
