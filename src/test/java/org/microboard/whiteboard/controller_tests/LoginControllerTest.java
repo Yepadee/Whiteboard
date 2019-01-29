6ff6283246cb3dd14a2b5c1c0dc496177a07ac11
James <james@hawkinsplace.co.uk>
2019-01-29 12:47:36 +0000

    Fixed login test

--- src/test/java/org/microboard/whiteboard/controller_tests/LoginControllerTest.java
+++ src/test/java/org/microboard/whiteboard/controller_tests/LoginControllerTest.java
@@ -5,12 +5,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.microboard.whiteboard.controllers.LoginController;
-import org.microboard.whiteboard.repositories.UserRepository;
-import org.microboard.whiteboard.services.ProjectService;
-import org.microboard.whiteboard.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -21,15 +17,6 @@ public class LoginControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
-	
-	@MockBean
-	private UserService userService;
-	
-	@MockBean
-	private UserRepository userRepository;
-	
-	@MockBean
-	private ProjectService projectService;
 	
 	@Test
 	public void test() throws Exception {
