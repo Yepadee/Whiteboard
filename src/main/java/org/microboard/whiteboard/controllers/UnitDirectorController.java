0267e54471e01d45e9faa9eb2d48e37e81f3282e
James <james@hawkinsplace.co.uk>
2019-01-30 11:41:50 +0000

    Added logger to unit director controller

--- src/main/java/org/microboard/whiteboard/controllers/UnitDirectorController.java
+++ src/main/java/org/microboard/whiteboard/controllers/UnitDirectorController.java
@@ -2,22 +2,33 @@ package org.microboard.whiteboard.controllers;
 
 import org.microboard.whiteboard.model.user.UnitDirector;
 import org.microboard.whiteboard.services.user.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/unit_director")
 public class UnitDirectorController {
 	
+	Logger logger = LoggerFactory.getLogger(UnitDirectorController.class);
+	
 	@Autowired
 	private UserService userService;
 	
 	@GetMapping("/new_project")
 	public String getNewProjectPage(Model model) {
+		return "new_project";
+	}
+	
+	@PostMapping("/new_project")
+	public String makeNewProjectPage(Model model) {
+		logger.info("New project added.");
 		return "new_project";
 	}
 	
